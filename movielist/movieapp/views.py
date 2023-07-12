3287ac287b4b3f5f0adfcff8467900e6007edd29
sonajaxn <sonajaison6@gmail.com>
2023-07-12 10:26:45 +0530

    new changes

--- movielist/movieapp/views.py
+++ movielist/movieapp/views.py
@@ -24,6 +24,7 @@ def add_movie(request):
         img=request.FILES['img']
         movie=Movie(name=name,desc=desc,year=year,img=img)
         movie.save()
+        return redirect('/')
     return render(request,'add.html')
 
 def update(request,id):
